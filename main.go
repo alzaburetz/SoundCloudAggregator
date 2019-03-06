517d5e9866df0fd1ee98838a1591878546605b2e
alzaburetz <michael322blazeit@gmail.com>
2019-03-06 08:32:55 +0300

    mp3 tagging, different formats

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"./models"
 	"encoding/json"
+	"github.com/mikkyang/id3-go"
 	"io/ioutil"
 	"log"
 	"math/rand"
@@ -18,21 +19,21 @@ var (
 	mu      sync.Mutex
 	cookie  string
 	cookies []string
-	client = "?client_id="
-	agent string
+	client  = "?client_id="
+	agent   string
 )
 
 //struct for encoding credentials
 type Cred struct {
 	ClientID string `json:"client_id"`
-	Agent string `json:"user_agent"`
+	Agent    string `json:"user_agent"`
 }
 
 //get client_id and user_agent(browser) from json file
 func getInfo() {
 	var credentials Cred
 	input, _ := ioutil.ReadFile("credentials.json")
-	err := json.Unmarshal(input,&credentials)
+	err := json.Unmarshal(input, &credentials)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -67,8 +68,8 @@ func getUsers(n int64) {
 			log.Fatal("Blocked\n")
 		}
 		bytes, _ := ioutil.ReadAll(resp)
-		json.Unmarshal(bytes,&user)
-		create(&user,i)
+		json.Unmarshal(bytes, &user)
+		create(&user, i)
 		log.Println(string(bytes))
 	}
 }
@@ -93,7 +94,7 @@ func getTrack(id int64) {
 	}
 	//set header to our agent and random cookie
 	req.Header.Set("User-Agent", agent)
-	req.Header.Set("Cookie",  cookies[rand.Intn(19)])
+	req.Header.Set("Cookie", cookies[rand.Intn(19)])
 	if req.StatusCode != 404 {
 		bytess, err := ioutil.ReadAll(req.Body)
 		json.Unmarshal(bytess, &track)
@@ -101,16 +102,33 @@ func getTrack(id int64) {
 			log.Fatal(err)
 		}
 		if track.Downloadable {
-			_ = os.Mkdir(track.Genre + "-genre", 0777)
-			req, _ := http.Get(track.DownloadURL + client)
-			reader := req.Body
-			info, _  := ioutil.ReadAll(reader)
-			_ = ioutil.WriteFile(track.Genre + "-genre/" + track.Title + ".mp3", info, 0666)
+			log.Printf("%s, %s, %s, %d", track.Genre, track.OriginalFormat, track.Title, track.ID)
+			go Download(track)
 		}
 	}
 
 }
 
+func Download(track models.Track) {
+	mu.Lock()
+	_ = os.Mkdir(track.Genre+"-genre", 0777)
+	req, _ := http.Get(track.DownloadURL + client)
+	reader := req.Body
+	info, _ := ioutil.ReadAll(reader)
+	_ = ioutil.WriteFile(track.Genre+"-genre/"+track.Title+"."+track.OriginalFormat, info, 0666)
+	if track.OriginalFormat == "mp3" {
+		mp3file, err := id3.Open(track.Genre + "-genre/" + track.Title + "." + track.OriginalFormat)
+		if err != nil {
+			log.Println(err)
+		}
+		defer mp3file.Close()
+		mp3file.SetTitle(track.Title)
+		mp3file.SetGenre(track.Genre)
+		mp3file.SetArtist(track.Creator.Username)
+		mp3file.SetYear(strconv.Itoa(track.ReleaseYear))
+	}
+	mu.Unlock()
+}
 
 func main() {
 	//fetch cookie. 20 for starters
@@ -119,10 +137,10 @@ func main() {
 		c, _ := http.Get("https://soundcloud.com")
 		c.Header.Set("User-Agent", agent)
 		cookie = c.Header.Get("Set-Cookie")
-		cookies = append(cookies,cookie)
+		cookies = append(cookies, cookie)
 	}
 	var i int64
-	for i = 0; i < 1000; i++ {
+	for i = 290; i < 1000; i++ {
 		getTrack(i)
 	}
 }
